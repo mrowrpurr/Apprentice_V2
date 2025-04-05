c440268743067772758af9492abc0d5f3081b491
Mrowr Purr <mrowr.purr@gmail.com>
2025-04-04 21:46:08 -0700

    Nice, nice, nice

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+# Bow
+
+```
+[2025-04-04 21:44:01.672] [log] [info] [apprentice_plugin.cpp:113] Player is drawing a bow!
+[2025-04-04 21:44:01.739] [log] [info] [apprentice_plugin.cpp:115] Player has attached an arrow to the bow!
+[2025-04-04 21:43:15.989] [log] [info] [apprentice_plugin.cpp:117] Player has fully drawn the bow!
+[2025-04-04 21:44:02.773] [log] [info] [apprentice_plugin.cpp:119] Player is releasing the bowstring!
+[2025-04-04 21:44:03.338] [log] [info] [apprentice_plugin.cpp:123] Player is preparing the next bow attack!
+```
--- src/apprentice_plugin.cpp
+++ src/apprentice_plugin.cpp
@@ -1,7 +1,63 @@
 #include <SkyrimScripting/Plugin.h>
 
+// BGSEventProcessedEvent
+// TESActivateEvent
+// TESActiveEffectApplyRemoveEvent
+// TESActorLocationChangeEvent
+// TESBookReadEvent
+// TESCellAttachDetachEvent
+// TESCellFullyLoadedEvent
+// TESCellReadyToApplyDecalsEvent
+// TESCombatEvent
+// TESContainerChangedEvent
+// TESDeathEvent
+// TESDestructionStageChangedEvent
+// TESEnterBleedoutEvent
+// TESEquipEvent
+// TESFormDeleteEvent
+// TESFurnitureEvent
+// TESGrabReleaseEvent
+// TESHitEvent
+// TESInitScriptEvent
+// TESLoadGameEvent
+// TESLockChangedEvent
+// TESMagicEffectApplyEvent
+// TESMagicWardHitEvent
+// TESMoveAttachDetachEvent
+// TESObjectLoadedEvent
+// TESObjectREFRTranslationEvent
+// TESOpenCloseEvent
+// TESPackageEvent
+// TESPerkEntryRunEvent
+// TESQuestInitEvent
+// TESQuestStageEvent
+// TESQuestStageItemDoneEvent
+// TESQuestStartStopEvent
+// TESResetEvent
+// TESResolveNPCTemplatesEvent
+// TESSceneEvent
+// TESSceneActionEvent
+// TESScenePhaseEvent
+// TESSellEvent
+// TESSleepStartEvent
+// TESSleepStopEvent
+// TESSpellCastEvent
+// TESPlayerBowShotEvent
+// TESTopicInfoEvent
+// TESTrackedStatsEvent
+// TESTrapHitEvent
+// TESTriggerEvent
+// TESTriggerEnterEvent
+// TESTriggerLeaveEvent
+// TESUniqueIDChangeEvent
+// TESWaitStartEvent
+// TESWaitStopEvent
+// TESSwitchRaceCompleteEvent
+// TESFastTravelEndEvent
+
 class EventSink : public RE::BSTEventSink<SKSE::CrosshairRefEvent>,
-                  public RE::BSTEventSink<RE::TESActivateEvent> {
+                  public RE::BSTEventSink<RE::TESActivateEvent>,
+                  public RE::BSTEventSink<RE::InputEvent*> {
 public:
     static EventSink& instance() {
         static EventSink singleton;
@@ -34,9 +90,56 @@ public:
         }
         return RE::BSEventNotifyControl::kContinue;
     }
+
+    RE::BSEventNotifyControl
+    ProcessEvent(RE::InputEvent* const* eventPtr, RE::BSTEventSource<RE::InputEvent*>*) {
+        if (!eventPtr) return RE::BSEventNotifyControl::kContinue;
+
+        auto* event = *eventPtr;
+        if (!event) return RE::BSEventNotifyControl::kContinue;
+
+        const auto* player = RE::PlayerCharacter::GetSingleton();
+        if (!player) return RE::BSEventNotifyControl::kContinue;
+
+        auto attackState = player->AsActorState()->GetAttackState();
+        if (attackState == RE::ATTACK_STATE_ENUM::kDraw) Log("Player is drawing a weapon!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kSwing) Log("Player is swinging a weapon!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kHit) Log("Player hit with a weapon!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kNextAttack)
+            Log("Player is preparing the next attack!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kFollowThrough)
+            Log("Player is following through with an attack!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kBash) Log("Player is bashing!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kBowDraw) Log("Player is drawing a bow!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kBowAttached)
+            Log("Player has attached an arrow to the bow!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kBowDrawn)
+            Log("Player has fully drawn the bow!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kBowReleasing)
+            Log("Player is releasing the bowstring!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kBowReleased)
+            Log("Player has released the bowstring!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kBowNextAttack)
+            Log("Player is preparing the next bow attack!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kBowFollowThrough)
+            Log("Player is following through with a bow attack!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kFire) Log("Player is firing a weapon!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kFiring)
+            Log("Player is in the process of firing!");
+        else if (attackState == RE::ATTACK_STATE_ENUM::kFired) Log("Player has fired a weapon!");
+
+        // if (event->GetEventType() == RE::INPUT_EVENT_TYPE::kButton) {
+        //     auto* buttonEvent = event->AsButtonEvent();
+        //     auto  dxScanCode  = buttonEvent->GetIDCode();
+        // }
+
+        return RE::BSEventNotifyControl::kContinue;
+    }
 };
 
-SKSEPlugin_Entrypoint {
+SKSEPlugin_OnDataLoaded {
+    RE::BSInputDeviceManager::GetSingleton()->AddEventSink((&EventSink::instance()));
+
     SKSE::GetCrosshairRefEventSource()->AddEventSink(&EventSink::instance());
 
     auto* eventSourceHolder = RE::ScriptEventSourceHolder::GetSingleton();
